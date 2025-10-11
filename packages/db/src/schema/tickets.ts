6f960593cb511f5b5c053c04a661f5a30f299ac2
Menno Jager <menno@menno-labs.com>
2025-10-11 19:34:49 +0700

    reorganize ticket stests

--- packages/db/src/schema/tickets.ts
+++ packages/db/src/schema/tickets.ts
@@ -14,9 +14,11 @@ export const tickets = pgTable("ticket", {
     onDelete: "cascade",
   }),
   priority: text("priority_id").references(() => priorities.id),
-  customerId: text("customer_id").references(() => customers.id, {
-    onDelete: "set null",
-  }),
+  customerId: text("customer_id")
+    .references(() => customers.id, {
+      onDelete: "set null",
+    })
+    .notNull(),
   assigneeId: text("assignee_id").references(() => users.id),
   createdAt,
   updatedAt,
